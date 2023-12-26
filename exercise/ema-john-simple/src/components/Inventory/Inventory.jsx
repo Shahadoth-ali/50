95e0a6ec0ecf22143b3de0d0bdfdcf4cb9731d84
Shahadoth-ali <md.sahadat.live@gmail.com>
2023-12-27 01:28:45 +0600

    third commit

---
+++ exercise/ema-john-simple/src/components/Inventory/Inventory.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Inventory = () => {
+  return (
+    <div>
+      <h3>This is inventory page.</h3>
+    </div>
+  )
+}
+
+export default Inventory
---
+++ exercise/ema-john-simple/src/components/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Orders = () => {
+  return (
+    <div>
+      <h3>This is orders page.</h3>
+    </div>
+  )
+}
+
+export default Orders
